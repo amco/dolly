56023205569f4253b6dc48db0fdbe01766521ccb
Adan Alvarado <adan.alvarado7@gmail.com>
2013-10-21 17:22:42 -0700

    Add delegators and command help

--- lib/dolly/cli.rb
+++ lib/dolly/cli.rb
@@ -1,10 +1,33 @@
+require 'forwardable'
+
 module Dolly
   class Dolly::CLI
+    extend Forwardable
+
+    HELP = 'Run `dolly generate` to generate view docs'
+
+    def_delegators :generator, :generate
 
     def initialize(*argv)
       command = argv.shift
       command = command.tr('-', '_') if command
+
+      if command && respond_to?(command) && argv.any?
+        send(command, argv)
+      elsif command && respond_to?(command)
+        send(command)
+      else
+        puts 'Command not found'
+        help
+      end
     end
 
+    def generator
+      @generator ||= MainGenerator.new
+    end
+
+    def help
+      puts HELP
+    end
   end
 end
