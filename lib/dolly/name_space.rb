7961385501df63a5ad6bf6f7d23a415b8deccfaf
javierg <javierg@amcoonline.net>
2013-11-07 11:44:12 -0800

    query by base id

--- lib/dolly/name_space.rb
+++ lib/dolly/name_space.rb
@@ -4,6 +4,13 @@ module Dolly
       model_name.param_key
     end
 
+    def base_id id
+      return id unless id =~ /^#{name_paramitized}/
+      base = id.split('/')
+      base.shift
+      base.join('/')
+    end
+
     def namespace id
       return id if id =~ /^#{name_paramitized}/
       "#{name_paramitized}/#{id}"
--- lib/dolly/query.rb
+++ lib/dolly/query.rb
@@ -17,7 +17,7 @@ module Dolly
       DESIGN_DOC = "dolly"
 
       def find *ids
-        response = default_view(keys: ids.map{ |id| [name_paramitized, id] }).parsed_response
+        response = default_view(keys: ids.map{ |id| [name_paramitized, base_id(id)] }).parsed_response
         ids.count > 1 ? Collection.new(response, name.constantize) : self.new.from_json(response)
       rescue NoMethodError => err
         if err.message == "undefined method `[]' for nil:NilClass"
