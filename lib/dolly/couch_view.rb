649f23241e87b696c2fe85d74c5045146f049e16
Jason King <jasonk@amcoonline.net>
2014-04-09 14:13:21 -0700

    Naming and flow cleanup

--- lib/dolly/couch_view.rb
+++ lib/dolly/couch_view.rb
@@ -2,9 +2,12 @@ module Dolly
   class CouchView
     extend Forwardable
 
+    ACTIVITY_CHECK_DELAY = 0.5 # seconds
+
     def_delegators :@doc, :[], :[]=, :to_json
 
     attr_reader :id, :type
+    attr :status
 
     def initialize id, file_type
       @id = "_design/#{id}"
@@ -51,14 +54,8 @@ module Dolly
       }
     end
 
-    def tmp_id
-      "#{id}_tmp"
-    end
-
-    def bare_json
-      d = to_hash.dup
-      d.delete :_rev
-      d.to_json
+    def copy_id
+      "#{id}_copy"
     end
 
     def bulk_save
@@ -67,59 +64,88 @@ module Dolly
       Document.bulk_document << model
     end
 
+    def saving?
+      status == :saving
+    end
+
     def document_save
-      pid = fork do
-        at_exit { puts "View #{id} was pushed and index recreated." }
-        push_design_start
-        sleep 1 while indexing_status.present?
-        push_document
+      # todo change to an exception?
+      return if saving?
+
+      fork do
+        self.status = :saving
+
+        create_and_index_copy
+        move_copy_to_self
+
+        self.status = :saved
+
+        # todo change this to be a logging statement?
+        puts "View #{id} was pushed and index recreated."
       end
       Process.wait
     end
 
-    def retrieve_tmp_doc
-      d = Document.database.get tmp_id
+    def create_and_index_copy
+      create_copy
+      index_copy
+    end
+
+    def create_copy
+      @copy_doc = @doc.except :_id, :_rev
+
+      @copy_doc[:_id] = copy_id
+      @copy_doc[:_rev] = existing_copy['_rev'] if existing_copy
+
+      push_document @copy_doc
+    end
+
+    def existing_copy
+      d = Document.database.get copy_id
       JSON.parse d.parsed_response
     rescue ResourceNotFound
       nil
     end
 
-    def tmp_doc
-      @tmp_doc ||= retrieve_tmp_doc
+    def push_document doc
+      Document.database.put doc[:_id], doc.to_json
     end
 
-    def push_design_start
-      clean_tmp if tmp_doc
-      @tmp_doc = JSON.parse update_tmp_design
-      Document.database.trigger_index( "#{tmp_id}/_view/#{query_trigger}" )
+    def index_copy
+      trigger_copy_index
+      wait_for_indexing
     end
 
-    def indexing_status
+    def trigger_copy_index
+      Document.database.trigger_index( "#{copy_id}/_view/#{any_view}" )
+    end
+
+    def any_view
+      views.keys.sample
+    end
+
+    def wait_for_indexing
+      while true
+        return if indexing_finished?
+        sleep ACTIVITY_CHECK_DELAY
+      end
+    end
+
+    def indexing_finished?
       running_tasks = Document.database.activity_tasks
-      running_tasks.detect{ |r| r["design_document"] == tmp_id }
+      running_tasks.detect{ |r| r["design_document"] == copy_id }
     end
 
-    def push_document id = nil
-      id ||= self[:_id]
-      Document.database.put id, to_json
-      clean_tmp
+    def move_copy_to_self
+      # todo refesh from the DB and only push if our _rev hasn't advanced?
+      push_document @doc
+
+    ensure
+      remove_document @copy_doc
     end
 
-    def update_tmp_design
-      Document.database.put( tmp_id, bare_json ).parsed_response
-    end
-
-    def tmp_rev
-      tmp_doc["rev"] || tmp_doc["_rev"]
-    end
-
-    def clean_tmp
-      Document.database.delete "#{tmp_id}?rev=#{tmp_rev}"
-      @indexing_starts = nil
-    end
-
-    def query_trigger
-      views.keys.first
+    def remove_document doc
+      Document.database.delete "#{doc[:_id]}?rev=#{doc[:_rev]}"
     end
 
   end
