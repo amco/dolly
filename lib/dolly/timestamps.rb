284e19e0eb2fc06c98203f2177e4addcf93345b8
seancookr <seanc@amcoonline.net>
2015-01-29 10:38:59 -0800

    ok javier, you win

--- lib/dolly/timestamps.rb
+++ lib/dolly/timestamps.rb
@@ -1,5 +1,4 @@
 require 'active_support/concern'
-require 'active_model/callbacks'
 
 module Dolly
   module Timestamps
@@ -7,13 +6,8 @@ module Dolly
 
     def timestamps!
       Dolly::Document.class_eval do
-        extend ActiveModel::Callbacks
-
-        define_model_callbacks :save
 
         property :created_at, :updated_at, class_name: DateTime
-
-        before_save :set_created_at, :set_updated_at
 
         def set_created_at
           self.created_at ||= DateTime.now
