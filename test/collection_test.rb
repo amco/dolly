eca03ed484e4fc0b73527deb095d9c2ce1795d56
seancookr <seanc@amcoonline.net>
2015-02-13 12:02:45 -0800

    working test

--- test/collection_test.rb
+++ test/collection_test.rb
@@ -7,8 +7,7 @@ end
 class CollectionTest < ActiveSupport::TestCase
 
   def setup
-    @json = '[{"id": "foo_bar/0", "key": "foo_bar", "value": 1, "doc": {"_id": "foo_bar/0", "_rev": "009eb36693218f8de0aea82273e0285b", "foo": "Foo B", "bar": "Bar B", "type": "foo_bar"}},
-             {"id": "foo_bar/1", "key": "foo_bar", "value": 1, "doc": {"_id": "foo_bar/1", "_rev": "ed78e55f6a4309b53255313647df9564", "foo": "Foo A", "bar": "Bar A", "type": "foo_bar"}}]'
+    @json = '{"total_rows":2,"offset":0,"rows":[{"id":"foo_bar/0","key":"foo_bar","value":1,"doc":{"_id":"foo_bar/0","_rev":"7f66379ac92eb6dfafa50c94bd795122","foo":"Foo B","bar":"Bar B","type":"foo_bar"}},{"id":"foo_bar/1","key":"foo_bar","value":1,"doc":{"_id":"foo_bar/1","_rev":"4d33cea0e55142c9ecc6a81600095469","foo":"Foo A","bar":"Bar A","type":"foo_bar"}}]}'
     @collection = Dolly::Collection.new @json, FooBar
   end
 
