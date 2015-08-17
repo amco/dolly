836f774a6725c0afd3b4fab8b1b35091277e1a21
seancookr <seanc@amcoonline.net>
2015-08-17 08:57:22 -0700

    do a refactor in a different pr

--- test/document_test.rb
+++ test/document_test.rb
@@ -1,5 +1,49 @@
 require 'test_helper'
-require 'support/base_documents'
+
+class BaseDolly < Dolly::Document; end
+
+class BarFoo < BaseDolly
+  property :a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l, :m, :n, :persist
+end
+
+class FooBar < BaseDolly
+  property :foo, :bar
+  property :with_default, default: 1
+  property :boolean, class_name: TrueClass, default: true
+  property :date, class_name: Date
+  property :time, class_name: Time
+  property :datetime, class_name: DateTime
+  property :is_nil, class_name: NilClass, default: nil
+
+  timestamps!
+end
+
+class Baz < Dolly::Document; end
+
+class FooBaz < Dolly::Document
+  property :foo, class_name: Hash, default: {}
+
+  def add_to_foo key, value
+    foo[key] ||= value
+    save!
+  end
+end
+
+class WithTime < Dolly::Document
+  property :created_at
+end
+
+class TestFoo < Dolly::Document
+  property :default_test_property, class_name: String, default: 'FOO'
+end
+
+class DocumentWithValidMethod < Dolly::Document
+  property :foo
+
+  def valid?
+    foo.present?
+  end
+end
 
 class DocumentTest < ActiveSupport::TestCase
   DB_BASE_PATH = "http://localhost:5984/test".freeze
