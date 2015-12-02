1cc2f7411fe7f49a60f64ceeddf3df0d394a6997
seancookr <seanc@amcoonline.net>
2015-12-02 09:51:15 -0800

    whoops

--- test/document_test.rb
+++ test/document_test.rb
@@ -448,7 +448,7 @@ class DocumentTest < ActiveSupport::TestCase
     assert FakeWeb.register_uri :put, /http:\/\/localhost:5984\/test\/base_dolly%2F.+/, body: save_response.to_json
     assert doc = BaseDolly.new
     assert doc.save
-    asssert resp = {ok: true, id: '79178957-96ff-40d9-9ecb-217fa35bdea7', rev: '2'}
+    assert resp = {ok: true, id: '79178957-96ff-40d9-9ecb-217fa35bdea7', rev: '2'}
     assert FakeWeb.register_uri :put, /http:\/\/localhost:5984\/test\/base_dolly\/79178957-96ff-40d9-9ecb-217fa35bdea7\/test.txt/, body: resp.to_json
     assert data = File.open("#{FileUtils.pwd}/test/support/test.txt").read
     assert doc.attach_file 'test.txt', 'text/plain', data
