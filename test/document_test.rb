f6df60f297c0cc8d99295e415cad8c91d1e4f581
seancookr <seanc@amcoonline.net>
2015-12-02 09:37:33 -0800

    clean up test for attachments

--- test/document_test.rb
+++ test/document_test.rb
@@ -444,14 +444,13 @@ class DocumentTest < ActiveSupport::TestCase
   end
 
   test 'attachments work properly' do
-    resp_one = {ok: true, id: "base_dolly/79178957-96ff-40d9-9ecb-217fa35bdea7", rev: "FF0000"}
-    FakeWeb.register_uri :put, /http:\/\/localhost:5984\/test\/base_dolly%2F.+/, body: resp_one.to_json
+    save_response = {ok: true, id: "base_dolly/79178957-96ff-40d9-9ecb-217fa35bdea7", rev: "1"}
+    FakeWeb.register_uri :put, /http:\/\/localhost:5984\/test\/base_dolly%2F.+/, body: save_response.to_json
     assert doc = BaseDolly.new
     assert doc.save
     resp = {ok: true, id: '79178957-96ff-40d9-9ecb-217fa35bdea7', rev: '2'}
     FakeWeb.register_uri :put, /http:\/\/localhost:5984\/test\/base_dolly\/79178957-96ff-40d9-9ecb-217fa35bdea7\/test.txt/, body: resp.to_json
     assert data = File.open("#{FileUtils.pwd}/test/support/test.txt").read
-    assert doc = BaseDolly.new
     assert doc.attach_file 'test.txt', 'text/plain', data
   end
 
