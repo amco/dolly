96c27c95e3e6a26320d623cdf168be1aa66d9e29
seancookr <seanc@amcoonline.net>
2016-06-21 13:45:19 -0700

    DRY

--- lib/dolly/document.rb
+++ lib/dolly/document.rb
@@ -105,15 +105,8 @@ module Dolly
     end
 
     def attach_file! file_name, mime_type, body, opts={}
-      if opts[:inline]
-        attachment_data = { file_name.to_s => { 'content_type' => mime_type,
-                                                'data'         => Base64.encode64(body)} }
-        doc['_attachments'] ||= {}
-        doc['_attachments'].merge! attachment_data
-        save
-      else
-        database.attach id_as_resource, CGI.escape(file_name), body, { 'Content-Type' => mime_type }
-      end
+      attach_file file_name, mime_type, body, opts={}
+      save if opts[:inline]
     end
 
     def attach_file file_name, mime_type, body, opts={}
