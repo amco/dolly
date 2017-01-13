dfbfb3d3bfca03677c7aa36f6700a9b9c96d30aa
seancookr <seanc@amcoonline.net>
2017-01-13 14:55:43 -0800

    add a autovivifying hash to simplify query building methods

--- lib/dolly/mango_query.rb
+++ lib/dolly/mango_query.rb
@@ -2,9 +2,11 @@ module Dolly
   class MangoQuery
 
     SELECTOR = 'selector'.freeze
+    EQ_OPERATOR = '$eq'
     IN_OPERATOR = '$in'.freeze
     GT_OPERATOR = '$gt'.freeze
     LT_OPERATOR = '$lt'.freeze
+    EM_OPERATOR = '$elemMatch'.freeze
     FIELDS_KEY = 'fields'.freeze
     SORT_KEY = 'sort'.freeze
     LIMIT_KEY = 'limit'.freeze
@@ -17,16 +19,20 @@ module Dolly
         in:    ->(name, value) { build_inclusion_selector name, value },
         gt:    ->(name, value) { build_greater_than_selector name, value },
         lt:    ->(name, value) { build_less_than_selector name, value },
+        em:    ->(name, value) { build_element_match_selector name, value },
+        [:em, :gt] => ->(name, value) { build_element_match_greater_than_selector name, value}
       }.freeze
     end
 
     def initialize proxy_class
       @proxy_class = proxy_class
-      @query = Hash.new
+      @query = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+      @query.compare_by_identity
     end
 
-    def selector name, operator, value
-      @query.deep_merge! select_operator_map[operator].call(name, value)
+    def selector name, *operator, value
+      operator = operator.count > 1 ? operator : operator.first
+      select_operator_map[operator].call(name, value)
     end
 
     def limit value
@@ -46,45 +52,27 @@ module Dolly
     private
 
     def build_equal_selector name, value
-      {
-        SELECTOR =>
-        {
-          name => value
-        }
-      }
+      @query[SELECTOR][name] = value
+    end
+
+    def build_element_match_selector name, value
+      @query[SELECTOR][name][EM_OPERATOR] = value
+    end
+
+    def build_element_match_greater_than_selector name, value
+      @query[SELECTOR][name][EM_OPERATOR][GT_OPERATOR] = value
     end
 
     def build_inclusion_selector name, value
-      {
-        SELECTOR =>
-        {
-          name => {
-            IN_OPERATOR => value
-          }
-        }
-      }
+      @query[SELECTOR][name][IN_OPERATOR] = value
     end
 
     def build_greater_than_selector name, value
-      {
-        SELECTOR =>
-        {
-          name => {
-            GT_OPERATOR => value
-          }
-        }
-      }
+      @query[SELECTOR][name][GT_OPERATOR] = value
     end
 
     def build_less_than_selector name, value
-      {
-        SELECTOR =>
-        {
-          name => {
-            LT_OPERATOR => value
-          }
-        }
-      }
+      @query[SELECTOR][name][LT_OPERATOR] = value
     end
   end
 end
--- test/mango_document_test.rb
+++ test/mango_document_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class MangoDoc < Dolly::Document
   property :year, :title, :char
   property :visible_to, class_name: Hash, default: Hash.new
+  property :default_collection, class_name: Array, default: Array.new
   timestamps!
 
   scope :by_year, ->(year) { selector('year', :eq, year) }
@@ -11,6 +12,7 @@ class MangoDoc < Dolly::Document
   scope :recent, -> { selector('created_at', :gt, 1.year.ago.to_s )}
   scope :old, -> { selector('created_at', :lt, 1.year.ago.to_s )}
   scope :by_visible_to_schools, -> (school_id) { selector('visible_to.schools', :eq, school_id) }
+  scope :collection_items_greater_than, -> (item) { selector('default_collection', :em, :gt, item)}
 end
 
 class MangoDocumentTest < ActiveSupport::TestCase
@@ -18,48 +20,54 @@ class MangoDocumentTest < ActiveSupport::TestCase
   class QueryIsBuiltTest < MangoDocumentTest
     test 'responds to the scoped method' do
       MangoDoc.scopes.keys.each do |k|
-        assert MangoDoc.respond_to? k
+        assert_respond_to MangoDoc, k
       end
     end
 
     test 'calling the scope builds the select query' do
-      query = MangoDoc.by_year(2000).query
-      expected = {"selector"=>{"year"=>2000}}
+      query = MangoDoc.by_year(2000).query.to_json
+      expected = {"selector"=>{"year"=>2000}}.to_json
 
       assert_equal expected, query
     end
 
     test 'the scopes are chainable' do
-      query = MangoDoc.by_title('A').by_year(2000).query
-      expected = {"selector"=>{"title"=>"A", "year"=>2000}}
+      query = MangoDoc.by_title('A').by_year(2000).query.to_json
+      expected = {"selector"=>{"title"=>"A", "year"=>2000}}.to_json
 
       assert_equal expected, query
     end
 
     test 'the scopes are chainable2' do
-      query = MangoDoc.by_title('A').by_year(2000).by_char('B').query
-      expected = {"selector"=>{"title"=>"A", "year"=>2000, "char"=>"B"}}
+      query = MangoDoc.by_title('A').by_year(2000).by_char('B').query.to_json
+      expected = {"selector"=>{"title"=>"A", "year"=>2000, "char"=>"B"}}.to_json
 
       assert_equal expected, query
     end
 
     test 'greater than selector builds query correctly' do
-      query = MangoDoc.by_title('A').recent.query
-      expected = {"selector"=>{"title"=>"A", "created_at"=>{"$gt"=>1.year.ago.to_s}}}
+      query = MangoDoc.by_title('A').recent.query.to_json
+      expected = {"selector"=>{"title"=>"A", "created_at"=>{"$gt"=>1.year.ago.to_s}}}.to_json
 
       assert_equal expected, query
     end
 
     test 'less than selector builds query correctly' do
-      query = MangoDoc.by_title('A').old.query
-      expected = {"selector"=>{"title"=>"A", "created_at"=>{"$lt"=>1.year.ago.to_s}}}
+      query = MangoDoc.by_title('A').old.query.to_json
+      expected = {"selector"=>{"title"=>"A", "created_at"=>{"$lt"=>1.year.ago.to_s}}}.to_json
 
       assert_equal expected, query
     end
 
     test 'selector can handle nested json object querys' do
-      query = MangoDoc.by_visible_to_schools('some_id').query
-      expected = {"selector"=>{"visible_to.schools"=>"some_id"}}
+      query = MangoDoc.by_visible_to_schools('some_id').query.to_json
+      expected = {"selector"=>{"visible_to.schools"=>"some_id"}}.to_json
+      assert_equal expected, query
+    end
+
+    test 'selector can handle multiple operators' do
+      query = MangoDoc.collection_items_greater_than(1).query.to_json
+      expected = {"selector"=>{"default_collection"=>{"$elemMatch"=>{"$gt"=>1}}}}.to_json
       assert_equal expected, query
     end
   end
