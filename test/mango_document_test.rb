5d2b941655b8f83ab45ff8f72f6439f9c9b3178b
seancookr <seanc@amcoonline.net>
2017-01-12 17:13:39 -0800

    add more tests to flesh out the api

--- test/mango_document_test.rb
+++ test/mango_document_test.rb
@@ -2,10 +2,13 @@ require 'test_helper'
 
 class MangoDoc < Dolly::Document
   property :year, :title, :char
+  timestamps!
 
   scope :by_year, ->(year) { selector('year', :eq, year) }
   scope :by_title, ->(title) { selector('title', :eq, title) }
   scope :by_char, ->(char) { selector('char', :eq, char) }
+  scope :recent, -> { selector('created_at', :gt, 1.year.ago.to_s )}
+  scope :old, -> { selector('created_at', :lt, 1.year.ago.to_s )}
 end
 
 class MangoDocumentTest < ActiveSupport::TestCase
@@ -33,6 +36,20 @@ class MangoDocumentTest < ActiveSupport::TestCase
     test 'the scopes are chainable2' do
       query = MangoDoc.by_title('A').by_year(2000).by_char('B').query
       expected = {"selector"=>{"title"=>"A", "year"=>2000, "char"=>"B"}}
+
+      assert_equal expected, query
+    end
+
+    test 'greater than selector builds query correctly' do
+      query = MangoDoc.by_title('A').recent.query
+      expected = {"selector"=>{"title"=>"A", "created_at"=>{"$gt"=>1.year.ago.to_s}}}
+
+      assert_equal expected, query
+    end
+
+    test 'less than selector builds query correctly' do
+      query = MangoDoc.by_title('A').old.query
+      expected = {"selector"=>{"title"=>"A", "created_at"=>{"$lt"=>1.year.ago.to_s}}}
 
       assert_equal expected, query
     end
