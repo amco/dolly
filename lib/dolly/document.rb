7bd26d70337346e71e0624633bee99d5738452f9
seancookr <seanc@amcoonline.net>
2017-02-15 10:09:38 -0800

    update

--- lib/dolly/document.rb
+++ lib/dolly/document.rb
@@ -154,7 +154,7 @@ module Dolly
       def mango_scope scope_name, scope
         self.mango_scopes ||= {}
         name = scope_name.to_sym
-        self.mango_scopes[name] = ->(query_object, args) { Mango::Scope.new(query_object, scope, args) }
+        self.mango_scopes[name] = ->(query_object, args=nil) { Mango::Scope.new(query_object, scope, args) }
 
         (class << self; self end).instance_eval do
           define_method name do |*args|
