ecde212fe6b9e27c26ffec7244d93c5a2b8c7894
seancookr <seanc@amcoonline.net>
2017-02-21 21:43:21 -0800

    move default config options to their proper place

--- lib/dolly/configuration.rb
+++ lib/dolly/configuration.rb
@@ -1,15 +1,17 @@
 require 'dolly/logger'
+
 module Dolly
   class Configuration
     attr_accessor :log_requests, :log_path, :log
 
     def initialize
       @log_requests = false
+      @log_path = $stdout
+      @log = :dolly
     end
 
     def logger
-      return Dolly::Logger.new(self) if %i/dolly default/.include?(log)
-      Rails.logger if log == :rails
+      return Rails.logger if log == :rails
       Dolly::Logger.new self
     end
   end
--- lib/dolly/logger.rb
+++ lib/dolly/logger.rb
@@ -1,8 +1,7 @@
 module Dolly
   class Logger < DelegateClass(::Logger)
     def initialize config
-      log_path = config.log_path || $stdout
-      logger = ::Logger.new(log_path)
+      logger = ::Logger.new(config.log_path)
       logger.progname = Dolly.name
       super logger
     end
