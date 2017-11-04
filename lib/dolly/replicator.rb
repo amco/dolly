b41c2218a19d21fd6b93cc63e235bfafa8629bbc
seancookr <seanc@amcoonline.net>
2017-11-03 19:56:04 -0700

    WIP

--- lib/dolly/replicator.rb
+++ lib/dolly/replicator.rb
@@ -1,7 +1,26 @@
 module Dolly
-  class Replicator
-    def initialize source, target
+  class SimpleReplicator
+    include Dolly::Connection
 
+    attr_reader :source_db, :target_db
+
+    def initialize source_db, target_db
+      @source_db = source_db
+      @target_db = target_db
     end
+
+    def replicate!
+      database.request :post, '/_replicate', {body: request_body}
+    end
+
+    private
+
+    def request_body
+      {
+        source: source_db,
+        target_db: target_db
+      }.to_json
+    end
+
   end
 end
