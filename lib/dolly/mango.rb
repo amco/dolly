c8e9a847bc486ff6c3b5c3c554b04449f3912a6a
Erick Fabian <fabianerickalfonso@gmail.com>
2019-06-20 12:29:19 -0500

    Refactors and improvements

--- lib/dolly/mango.rb
+++ lib/dolly/mango.rb
@@ -23,15 +23,29 @@ module Dolly
     DESIGN = "_find"
 
     def find_by(query, opts = {})
+      build_model_from_doc(find_doc_by(query, opts))
+    end
+
+    def find_doc_by(query, opts = {})
       opts.merge!(limit: 1)
       perform_query(build_query(query, opts))[:docs].first
     end
 
     def where(query, opts = {})
+      docs_where(query, opts).map do |doc|
+        build_model_from_doc(doc)
+      end
+    end
+
+    def docs_where(query, opts = {})
       perform_query(build_query(query, opts))[:docs]
     end
 
     private
+
+    def build_model_from_doc(doc)
+      self.new(doc.slice(*self.property_keys))
+    end
 
     def perform_query(structured_query)
       connection.post(DESIGN, structured_query)
@@ -44,17 +58,16 @@ module Dolly
     end
 
     def build_selectors(query)
-      query.map do |key, values|
-        inner_key = values.keys.first
-        inner_value = values.values.first
+      query.each_with_object(Hash.new) do |(key, value), hsh|
+        inner_key = value.keys.first
+        inner_value = value.values.first
         next unless AVAILABLE_SELECTORS.include?(inner_key)
-
-        {
+        hsh.merge!({
           "#{key}" => {
             build_key(inner_key) => inner_value
           }
-        }
-      end.compact.inject(:merge)
+        })
+      end
     end
 
     def build_key(key)
