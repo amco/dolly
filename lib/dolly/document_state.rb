e87b9788f87667ed15b46559823f2a05f3891e06
Erick Fabian <fabianerickalfonso@gmail.com>
2019-07-23 17:34:43 -0500

    Add back type to dolly document

--- lib/dolly/document_state.rb
+++ lib/dolly/document_state.rb
@@ -6,7 +6,7 @@ module Dolly
 
     def save(options = {})
       return false unless options[:validate] == false || valid?
-      set_type if typed? && doc_type.nil?
+      set_type if typed? && type.nil?
       write_timestamps(persisted?)
       after_save(connection.put(id, doc))
     end
