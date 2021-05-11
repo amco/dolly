5aadf265c1470c3e508afbded1fbe631d2fd1bdb
ErickFabian <fabianerickalfonso@gmail.com>
2021-05-11 09:13:54 -0500

    Make property manager test more robust

--- test/property_manager_test.rb
+++ test/property_manager_test.rb
@@ -2,13 +2,17 @@ require 'test_helper'
 
 class TestDoc < Dolly::Document
   property :name, class_name: String
+  property :email, class_name: String
+  property :last_name, class_name: String
 end
 
 class PropertyManagerTest < Test::Unit::TestCase
   test 'write_attribute with nil value' do
-    doc = TestDoc.new(name: 'name')
+    doc = TestDoc.new(name: 'name', last_name: nil, email: 'does not change')
     assert_equal(doc.name, 'name')
     doc.update_properties(name: nil)
     assert_equal(doc.name, nil)
+    assert_equal(doc.last_name, nil)
+    assert_equal(doc.email, 'does not change')
   end
 end
