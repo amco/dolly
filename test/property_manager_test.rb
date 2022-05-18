9a4c3b5453de079e50a540bf74b5342a7ce6a248
javierg <javierg@amcoonline.net>
2022-05-18 11:17:41 -0700

    fix truthy wrong return

--- test/property_manager_test.rb
+++ test/property_manager_test.rb
@@ -4,15 +4,17 @@ class TestDoc < Dolly::Document
   property :name, class_name: String
   property :email, class_name: String
   property :last_name, class_name: String
+  property :active, class_name: TrueClass
 end
 
 class PropertyManagerTest < Test::Unit::TestCase
   test 'write_attribute with nil value' do
-    doc = TestDoc.new(name: 'name', last_name: nil, email: 'does not change')
+    doc = TestDoc.new(name: 'name', last_name: nil, email: 'does not change', active: 'true')
     assert_equal(doc.name, 'name')
     doc.update_properties(name: nil)
     assert_equal(doc.name, nil)
     assert_equal(doc.last_name, nil)
     assert_equal(doc.email, 'does not change')
+    assert_equal(doc.active, true)
   end
 end
